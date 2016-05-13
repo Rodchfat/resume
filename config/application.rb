0a06a57f7c47ed37ecb4dd7e0a5f0e64fc2c35c2
Rod c <raadce@gmail.com>
2016-05-13 17:27:04 +0000

    final touches and deploy to heorku

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "<example.tld>",
+  user_name: "<username>",
+  password: "<password>",
+  authentication: :plain,
+  enable_starttls_auto: true
+}
 
+config.action_mailer.default_url_options = {
+  host: "yourdomain.tld"
+}
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
